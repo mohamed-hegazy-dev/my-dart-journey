0c45fab8cc6c86221f647591ca91dee2fda2b553
Mohamed Hegazy <132461548+mohamed-hegazy-dev@users.noreply.github.com>
2026-06-20 18:56:14 +0300

    Create day01_receipt.dart

---
+++ day01_receipt.dart
@@ -0,0 +1,18 @@
+import'dart:math';
+main(){
+print('---welcome to our tech store---\n');
+print('Customer Name: Mohamed'+' '+'Hegazy\n');
+print('Items Purchased:');
+print('1. Mobile screen 120\$');
+print('2. Type-c cable 30\$');
+print('3.Phone case 50\$\n');
+print('\t------------------------------------------------');
+print('Total before discount: 200\$');
+print('Invoice Summary:');
+print('The final price after 10% discount is:${(120+30+50)*0.9}\$');
+print('Thank you for shopping with us!');
+
+
+
+
+}
